c84635260988090493bee9201be40656383e6dc3
david benitez <flio034@gmail.com>
2024-03-08 20:33:19 +0100

    Uploading correct inital files

---
+++ ProjectSailor/Source/ProjectSailor/ProjectSailorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSailorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectSailorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectSailorGameMode();
+};
+
+
+
