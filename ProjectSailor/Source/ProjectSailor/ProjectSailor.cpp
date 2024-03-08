f7a41fd947d0cc469d9bd224b4bca8c9c1179bfb
david benitez <flio034@gmail.com>
2024-03-08 20:22:55 +0100

    Removing unnecessary folder

--- ProjectSailor/Source/ProjectSailor/ProjectSailor.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjectSailor.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSailor, "ProjectSailor" );
- 
