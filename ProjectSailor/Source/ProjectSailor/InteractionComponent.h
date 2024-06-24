0298442c5d7b174b2eda01795d781232c9562d9c
david benitez <flio034@gmail.com>
2024-06-24 03:12:37 -0500

    New level cave (#23)
    * Create Level2.umap
    
    * pasillo creado
    
    * escaleras
    
    * paredes
    
    * segunda planta
    
    * parte 2 cueva con habitaciones arriba
    
    * Update Level2.umap
    
    * Pasillo duplicado
    
    * paredes y techo pasillo
    
    * setas en el techo
    
    * optimization
    
    * optimization level 1
    
    * level 2 optimizated except walls and roof
    
    * static lights
    
    * Update Level2.umap
    
    ---------
    
    Co-authored-by: Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>

--- ProjectSailor/Source/ProjectSailor/InteractionComponent.h
+++ ProjectSailor/Source/ProjectSailor/InteractionComponent.h
@@ -29,6 +29,7 @@ public:
     // Called every frame
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
     UFUNCTION() void PerformRaycast();
+    
 private:
     UWorld* World{ nullptr };
    
