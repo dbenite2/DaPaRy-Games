c79936ffb5f24c03c3713687f1d27b36925884ff
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-07-04 23:07:05 +0200

    key fell off the tree on the beach

---
+++ ProjectSailor/Source/ProjectSailor/KeyBeach.cpp
@@ -0,0 +1,39 @@
+// Property of DaPaRy Games
+
+
+#include "KeyBeach.h"
+
+// Sets default values
+AKeyBeach::AKeyBeach()
+{
+	// Set this actor to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create and initialize the mesh component
+	KeyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("KeyMesh"));
+	RootComponent = KeyMesh;
+	KeyMesh->SetSimulatePhysics(false);
+	KeyMesh->SetCollisionProfileName(TEXT("PhysicsActor"));
+
+	
+}
+
+// Called when the game starts or when spawned
+void AKeyBeach::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKeyBeach::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AKeyBeach::ActivateKeyPhysics()
+{
+	KeyMesh->SetSimulatePhysics(true);
+}
+
---
+++ ProjectSailor/Source/ProjectSailor/KeyBeach.h
@@ -0,0 +1,31 @@
+// Property of DaPaRy Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KeyBeach.generated.h"
+
+UCLASS()
+class PROJECTSAILOR_API AKeyBeach : public AActor
+{
+	GENERATED_BODY()
+    
+public:    
+	// Sets default values for this actor's properties
+	AKeyBeach();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:    
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* KeyMesh;
+
+	// Function to initialize the key position and physics
+	void ActivateKeyPhysics();
+};
