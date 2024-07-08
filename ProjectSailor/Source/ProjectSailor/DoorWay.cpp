c624ae36c7ab30fe1113f42d2ab4d2999f0cb799
richi_mf <ricardomoralesfer@yahoo.com>
2024-07-08 20:22:14 +0200

    DPR-99:Create Hub Level

---
+++ ProjectSailor/Source/ProjectSailor/DoorWay.cpp
@@ -0,0 +1,27 @@
+// Property of DaPaRy Games
+
+
+#include "DoorWay.h"
+
+// Sets default values
+ADoorWay::ADoorWay()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADoorWay::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoorWay::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
