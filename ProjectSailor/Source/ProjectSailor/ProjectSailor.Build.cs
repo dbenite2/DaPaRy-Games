d246f1a069879032168615b09e3b576a5b8aa342
david benitez <flio034@gmail.com>
2024-07-23 09:44:40 -0500

    Adding level transition VFX, music to the first level (#70)

--- ProjectSailor/Source/ProjectSailor/ProjectSailor.Build.cs
+++ ProjectSailor/Source/ProjectSailor/ProjectSailor.Build.cs
@@ -10,6 +10,6 @@ public class ProjectSailor : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "CableComponent" });
-		PrivateDependencyModuleNames.AddRange(new string[] { "CableComponent" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "CableComponent", "Niagara" });
 	}
 }
