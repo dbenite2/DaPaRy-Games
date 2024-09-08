062327b2635e736d3ea2ccbf37156e66ad8372ce
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-09-08 19:47:16 +0200

    trying to fix fatal error changeLevels

--- ProjectSailor/Source/ProjectSailor/LevelStreamerActor.cpp
+++ ProjectSailor/Source/ProjectSailor/LevelStreamerActor.cpp
@@ -29,9 +29,15 @@ void ALevelStreamerActor::Tick(float DeltaTime)
 void ALevelStreamerActor::OverlapBegins(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                         UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 	const ACharacter* MyCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
-	if (OtherActor == MyCharacter && !LevelToLoadReference.IsNull()) {
-		UGameplayStatics::OpenLevelBySoftObjectPtr(this, LevelToLoadReference);
+
+	if(MyCharacter)
+	{
+		if (OtherActor == MyCharacter && !LevelToLoadReference.IsNull())
+		{
+			UGameplayStatics::OpenLevelBySoftObjectPtr(this, LevelToLoadReference);
+		}
 	}
+	
 }
 
 
